bd2b0d1f73895647dc03a4b7e95515c46fb2471d
R3lativ3 <josebarrundia21@gmail.com>
2021-06-05 17:37:14 -0600

    lele

--- models/sockets.js
+++ models/sockets.js
@@ -1,3 +1,4 @@
+const Marcadores = require("./Marcadores");
 
 
 class Sockets {
@@ -6,20 +7,25 @@ class Sockets {
 
         this.io = io;
 
+        this.marcadores = new Marcadores()
+
         this.socketEvents();
     }
 
     socketEvents() {
         // On connection
         this.io.on('connection', ( socket ) => {
+            socket.emit('marcadores-activos', this.marcadores.activos)
+    
+            socket.on('marcador-nuevo', (marcador) => {
+                this.marcadores.agregarMarcador(marcador)
+                socket.broadcast.emit('marcador-nuevo', marcador)
+            })
 
-            // Escuchar evento: mensaje-to-server
-            socket.on('mensaje-to-server', ( data ) => {
-                console.log( data );
-                
-                this.io.emit('mensaje-from-server', data );
-            });
-            
+            socket.on('marcador-actualizado', (marcador) => {
+                this.marcadores.actualizarMarcador( marcador )
+                socket.broadcast.emit('marcador-actualizado', marcador)
+            })
         
         });
     }
